e596ac3a11f0df4e6ae19867f604f655e75545cd
Kyler Wong <wsm.kyler@gmail.com>
2020-07-14 05:37:44 +0800

    Return not found code when link does not exist

--- functions/links/main.go
+++ functions/links/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -16,8 +17,11 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	if err != nil {
 		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, err
 	}
-	headers := map[string]string{"Location": link.OriginalLink}
-	return events.APIGatewayProxyResponse{StatusCode: http.StatusFound, Headers: headers}, nil
+	if originalLink := link.OriginalLink; len(strings.TrimSpace(originalLink)) > 0 {
+		headers := map[string]string{"Location": link.OriginalLink}
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusFound, Headers: headers}, nil
+	}
+	return events.APIGatewayProxyResponse{StatusCode: http.StatusNotFound}, nil
 }
 
 func main() {
